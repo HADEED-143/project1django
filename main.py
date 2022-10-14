2a8252a529a6fab8c5fa6c349472a37769b8de45
Hadeed Zubair <60264256+HADEED-143@users.noreply.github.com>
2022-10-14 04:37:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class queuedata(object):
 
+        def __init__(self):
+                casesdata = requests.get('http://127.0.0.1:8000/API/cases/')
+                labdata = requests.get('http://127.0.0.1:8000/API/labs/')
+                quarantinedata = requests.get('http://127.0.0.1:8000/API/quarantine/')
+                sqliteseqdata = requests.get('http://127.0.0.1:8000/API/sqliteseq/')
+                summerydata = requests.get('http://127.0.0.1:8000/API/summery/')
+                vaccinedata = requests.get('http://127.0.0.1:8000/API/vaccine/')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+                queue = [casesdata, labdata, quarantinedata, sqliteseqdata, summerydata, vaccinedata]
 
+                self.element = [queue]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        def enqueue(self, queue):
+                self.element.append(queue)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        def dequeue(self):
+                return self.element.pop(0)
+
+        def rear(self):
+                return self.element[-1]
+
+        def front(self):
+                return self.element[0]
+
+        def isEmpty(self):
+                return len(self.element) == 0
+
+        def display(self):
+                print(self.queue)
+
+                while True:
+                        print("Select operation \n1: Add to queue data \n 2: Remove from queue \n 3: LiFO queue  \n 4: Fifo queue \n 5: check empty queue \n 6: display queue \n 7: Exit" )
+                        choice = int(input())
+
+                        if choice == 1:
+                                self.enqueue()
+                        elif choice == 2:
+                                self.dequeue()
+                        elif choice == 3:
+                                self.rear()
+                        elif choice == 4:
+                                self.front()
+                        elif choice == 5:
+                                self.isEmpty()
+                        elif choice == 6:
+                                self.display()
+                        else:
+                                print("Enter correct operations")
+                                break
